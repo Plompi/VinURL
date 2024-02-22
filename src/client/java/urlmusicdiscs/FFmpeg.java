a2cd4cf6b127daadd81e9d3503141beb38e4c041
Plompi <Plompiimail@gmail.com>
2024-02-22 01:26:08 +0100

    changed the source for downloading ffmpeg, dont need to save .zip file anymore -> extracts file directly from zip file and saves it

--- src/client/java/urlmusicdiscs/FFmpeg.java
+++ src/client/java/urlmusicdiscs/FFmpeg.java
@@ -9,11 +9,11 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class FFmpeg {
-    private static final File Directory = FabricLoader.getInstance().getConfigDir().resolve("urlmusicdiscs/ffmpeg/").toAbsolutePath().toFile();
+    private static final String OS = SystemUtils.IS_OS_LINUX ? "linux" : SystemUtils.IS_OS_MAC ? "macos" : "win";
+    private static final File Directory = FabricLoader.getInstance().getConfigDir().resolve("urlmusicdiscs/ffmpeg").toFile();
     private static final Path FilePath = Directory.toPath().resolve(SystemUtils.IS_OS_WINDOWS ? "ffmpeg.exe" : "ffmpeg");
     static void checkForExecutable() throws IOException, URISyntaxException {
 
@@ -21,43 +21,20 @@ public class FFmpeg {
             throw new IOException();
         }
 
-
         if (!FilePath.toFile().exists()) {
-            File zipFile = new File(Directory, "ffmpeg.zip");
 
-            if (!zipFile.exists()) {
-                try (InputStream in = getDownloadInputStream()) {
-                    Files.copy(in, zipFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            try (ZipInputStream zipInput = new ZipInputStream(getDownloadInputStream())) {
+                zipInput.getNextEntry();
+                Files.copy(zipInput, FilePath, StandardCopyOption.REPLACE_EXISTING);
+
+                if (SystemUtils.IS_OS_UNIX) {
+                    Runtime.getRuntime().exec(new String[]{"chmod", "+x", FilePath.toString()});
                 }
             }
-
-            try (ZipInputStream zipInput = new ZipInputStream(new FileInputStream(zipFile))) {
-                ZipEntry zipEntry;
-                while ((zipEntry = zipInput.getNextEntry()) != null) {
-                    if (zipEntry.getName().endsWith(SystemUtils.IS_OS_WINDOWS ? "ffmpeg.exe" : "ffmpeg")) {
-                        Files.copy(zipInput, FilePath, StandardCopyOption.REPLACE_EXISTING);
-                        break;
-                    }
-                }
-            }
-            if (SystemUtils.IS_OS_UNIX) {
-                Runtime.getRuntime().exec(new String[]{"chmod", "+x", FilePath.toString()});
-            }
-
-            if (!zipFile.delete()){throw new IOException();}
         }
     }
 
     private static InputStream getDownloadInputStream() throws IOException, URISyntaxException {
-
-        if (SystemUtils.IS_OS_LINUX) {
-            //Discord CDN changes to be temporary -> might deprecate in the future
-            return new URI("https://cdn.discordapp.com/attachments/1067144249612714036/1175188765711552592/ffmpeg.zip").toURL().openStream();
-        } else if (SystemUtils.IS_OS_MAC) {
-            return new URI("https://evermeet.cx/ffmpeg/ffmpeg-6.1.zip").toURL().openStream();
-        } else if (SystemUtils.IS_OS_WINDOWS) {
-            return new URI("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip").toURL().openStream();
-        }
-        throw new UnsupportedOperationException("Unsupported operating system.");
+        return new URI(String.format("https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-%s-64.zip",OS)).toURL().openStream();
     }
 }
--- src/client/java/urlmusicdiscs/YoutubeDL.java
+++ src/client/java/urlmusicdiscs/YoutubeDL.java
@@ -15,8 +15,9 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class YoutubeDL {
-    private static final File Directory = FabricLoader.getInstance().getConfigDir().resolve("urlmusicdiscs/youtubedl").toAbsolutePath().toFile();
-    private static final Path FilePath = Directory.toPath().resolve(SystemUtils.IS_OS_WINDOWS ? "yt-dlp.exe" : "yt-dlp");
+    private static final String FileName = SystemUtils.IS_OS_LINUX ? "yt-dlp_linux" : SystemUtils.IS_OS_MAC ? "yt-dlp_macos" : "yt-dlp.exe";
+    private static final File Directory = FabricLoader.getInstance().getConfigDir().resolve("urlmusicdiscs/youtubedl").toFile();
+    private static final Path FilePath = Directory.toPath().resolve(FileName);
     static void checkForExecutable() throws IOException, URISyntaxException {
 
         if(!Directory.exists() && !Directory.mkdirs()) {
@@ -36,19 +37,11 @@ public class YoutubeDL {
     }
 
     static void executeYoutubeDLCommand(String ... arguments) throws IOException, InterruptedException {
-        // https://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
         Runtime.getRuntime().exec(Stream.concat(Stream.of(FilePath.toString()),Arrays.stream(arguments)).toArray(String[]::new)).waitFor();
     }
 
     private static InputStream getDownloadInputStream() throws IOException, URISyntaxException {
+        return new URI(String.format("https://github.com/yt-dlp/yt-dlp/releases/latest/download/%s",FileName)).toURL().openStream();
 
-        if (SystemUtils.IS_OS_LINUX) {
-            return new URI("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux").toURL().openStream();
-        } else if (SystemUtils.IS_OS_MAC) {
-            return new URI("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos").toURL().openStream();
-        } else if (SystemUtils.IS_OS_WINDOWS) {
-            return new URI("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe").toURL().openStream();
-        }
-        throw new UnsupportedOperationException("Unsupported operating system.");
     }
 }
