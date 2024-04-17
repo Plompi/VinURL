b9587962e88631aa9cdfcd44a7176b6ac1b2920d
Plompi <Plompiimail@gmail.com>
2024-04-17 13:30:04 +0200

    refactoring of Commands.java
    (cherry picked from commit e4a07301867e754ae9428c1c2380aed171dad577)

--- src/client/java/com/vinurl/cmd/Commands.java
+++ src/client/java/com/vinurl/cmd/Commands.java
@@ -1,12 +1,14 @@
 package com.vinurl.cmd;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.vinurl.VinURL;
 import com.vinurl.exe.FFmpeg;
 import com.vinurl.exe.YoutubeDL;
 import io.wispforest.owo.config.ui.ConfigScreen;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.MinecraftClient;
@@ -27,53 +29,54 @@ public class Commands {
 
 	public static void register() {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal(VinURL.MOD_ID)
-				.then(ClientCommandManager.literal("delete")
-						.executes(context -> {
-							try {
-								FileUtils.deleteDirectory(VinURL.VINURLPATH.resolve("client_downloads").toFile());
-								context.getSource().sendFeedback(Text.literal("Deleted all Audio Files"));
-								return 1;
-							} catch (IOException e) {
-								context.getSource().sendFeedback(Text.literal("Deleted only non active Audio Files"));
-								return 0;
-							}
-						})
-				)
-
-				.then(ClientCommandManager.literal("update").executes(context -> {
-					context.getSource().sendFeedback(Text.literal("Checking for Updates..."));
-					CompletableFuture.runAsync(() -> {
-						if (YoutubeDL.getInstance().checkForUpdates() | FFmpeg.getInstance().checkForUpdates()) {
-							context.getSource().sendFeedback(Text.literal("Successfully updated Executables"));
-						} else {
-							context.getSource().sendFeedback(Text.literal("No Updates found"));
-						}
-					});
-					return 1;
-				}))
-
-				.then(ClientCommandManager.literal("config").executes(context -> {
-					ConfigScreen screen = Objects.requireNonNull(ConfigScreen.getProvider(VinURL.MOD_ID)).apply(null);
-					MinecraftClient.getInstance().send(() -> MinecraftClient.getInstance().setScreen(screen));
-					return 0;
-				}))
-
-				.then(ClientCommandManager.literal("set")
-						.then(argument("url", StringArgumentType.greedyString())
-								.executes(context -> {
-									String url = StringArgumentType.getString(context, "url");
-									ItemStack heldItem = context.getSource().getPlayer().getStackInHand(Hand.MAIN_HAND);
-
-									if (heldItem.getItem() != CUSTOM_RECORD) {
-										context.getSource().sendFeedback(Text.of("VinURL-Disc needed in Main Hand"));
-										return 0;
-									}
-
-									PacketByteBuf bufInfo = PacketByteBufs.create();
-									bufInfo.writeString(url);
-									ClientPlayNetworking.send(VinURL.CUSTOM_RECORD_SET_URL, bufInfo);
-									return 1;
-								})))
+				.then(ClientCommandManager.literal("delete").executes(Commands::deleteAudioFiles))
+				.then(ClientCommandManager.literal("update").executes(Commands::updateExecutables))
+				.then(ClientCommandManager.literal("config").executes(Commands::openConfig))
+				.then(ClientCommandManager.literal("set").then(argument("url", StringArgumentType.string()).executes(Commands::setURLToDisc)))
 		));
+	}
+
+	private static int deleteAudioFiles(CommandContext<FabricClientCommandSource> ctx) {
+		try {
+			FileUtils.deleteDirectory(VinURL.VINURLPATH.resolve("client_downloads").toFile());
+			ctx.getSource().sendFeedback(Text.literal("Deleted all Audio Files"));
+			return 1;
+		} catch (IOException e) {
+			ctx.getSource().sendFeedback(Text.literal("Deleted only non active Audio Files"));
+			return 0;
+		}
+	}
+
+	private static int updateExecutables(CommandContext<FabricClientCommandSource> ctx) {
+		ctx.getSource().sendFeedback(Text.literal("Checking for Updates..."));
+		CompletableFuture.runAsync(() -> {
+			if (YoutubeDL.getInstance().checkForUpdates() | FFmpeg.getInstance().checkForUpdates()) {
+				ctx.getSource().sendFeedback(Text.literal("Successfully updated Executables"));
+			} else {
+				ctx.getSource().sendFeedback(Text.literal("No Updates found"));
+			}
+		});
+		return 1;
+	}
+
+	private static int openConfig(CommandContext<FabricClientCommandSource> ctx) {
+		ConfigScreen screen = Objects.requireNonNull(ConfigScreen.getProvider(VinURL.MOD_ID)).apply(null);
+		MinecraftClient.getInstance().send(() -> MinecraftClient.getInstance().setScreen(screen));
+		return 0;
+	}
+
+	private static int setURLToDisc(CommandContext<FabricClientCommandSource> ctx) {
+		String url = StringArgumentType.getString(ctx, "url");
+		ItemStack heldItem = ctx.getSource().getPlayer().getStackInHand(Hand.MAIN_HAND);
+
+		if (heldItem.getItem() != CUSTOM_RECORD) {
+			ctx.getSource().sendFeedback(Text.of("VinURL-Disc needed in Main Hand"));
+			return 0;
+		}
+
+		PacketByteBuf bufInfo = PacketByteBufs.create();
+		bufInfo.writeString(url);
+		ClientPlayNetworking.send(VinURL.CUSTOM_RECORD_SET_URL, bufInfo);
+		return 1;
 	}
 }
