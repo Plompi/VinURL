b9211aec07fe179c9e5ea7731734dfc544d48f4e
Philipp Schlonski <philipp.schlonski@campus.tu-berlin.de>
2025-10-24 01:27:15 +0200

    port to 1.21.5

--- src/main/java/com/vinurl/gui/URLDiscScreen.java
+++ src/main/java/com/vinurl/gui/URLDiscScreen.java
@@ -1,6 +1,5 @@
 package com.vinurl.gui;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.vinurl.client.SoundManager;
 import com.vinurl.client.VinURLClient;
 import com.vinurl.exe.Executable;
@@ -26,7 +25,6 @@ public class URLDiscScreen extends BaseUIModelScreen<StackLayout> {
 	private int duration;
 
 	private final ButtonComponent.Renderer SIMULATE_BUTTON_TEXTURE = (matrices, button, delta) -> {
-		RenderSystem.enableDepthTest();
 		ResourceLocation texture = !simulate ? (button.active && button.isHovered() ?
 			SIMULATE_BUTTON_HOVER_ID :
 			SIMULATE_BUTTON_ID) :
@@ -35,7 +33,6 @@ public class URLDiscScreen extends BaseUIModelScreen<StackLayout> {
 	};
 
 	private final ButtonComponent.Renderer LOCK_BUTTON_TEXTURE = (matrices, button, delta) -> {
-		RenderSystem.enableDepthTest();
 		ResourceLocation texture = lock ?
 			LOCK_BUTTON_ID :
 			LOCK_BUTTON_DISABLED_ID;
